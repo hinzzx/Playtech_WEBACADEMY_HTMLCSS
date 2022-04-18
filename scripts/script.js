360b0706a3ebc78508ce6ac7444962e6c3ad4703
unknown <abvsuxhard@gmail.com>
2022-04-19 00:22:58 +0300

    robotchetata veche sa mujki i jenski, i ima buton za dobavqne na sekcii

--- scripts/script.js
+++ scripts/script.js
@@ -38,6 +38,7 @@ let Robots = [
 ];
 
 let newRobots = [];
+let createdRobots = [];
 
 console.log(robot2.frazichka); // Тест дали работи
 
@@ -77,7 +78,7 @@ form.addEventListener("reset", (e) => {
     alert(
       `Вашите роботи са: \n${newRobots.map(
         (robot) =>
-         `\n        Name: ${robot.name} 
+          `\n        Name: ${robot.name} 
           \n        Color: ${robot.color} 
           \n        Type: ${robot.type} 
           \n ------------------------------- \n
@@ -107,42 +108,155 @@ form.addEventListener("reset", (e) => {
 const body = document.querySelector("body");
 const robotSectionOne = document.querySelector("#robot-section-1");
 const robotDrawing = document.querySelector("robot-drawing");
+const small = document.querySelector("small");
 
 // добавя класа d-none (display: none) на секцията с роботчето, при зареждане на страницата
 
-// function changeClassOnLoad() {
-//   robotSectionOne.className = robotSectionOne.className + " d-none";
-// }
+function changeClassOnLoad() {
+  robotSectionOne.className = robotSectionOne.className + " d-none";
+}
 
 // create new dynamic section with robot every time we submit the form
 
-// function createRobot() {
+function createRobot() {
+  if (newRobots && newRobots.length > 0) {
+    newRobots.forEach((robotche) => {
+      
+      // Ако роботчето е женско, му слагаме клас skirt (пола)
 
-//   if (newRobots !== undefined || newRobots !== null) {
-//     newRobots.map((robotche) => {
-//     let newRobotSection = document.createElement("section");
-//     newRobotSection.className = "container-1";
-//     newRobotSection.id = `robot-section-${newRobots.indexOf(robotche)}`;
-//     body.appendChild(newRobotSection);
-//     body.insertBefore(newRobotSection, robotSectionOne);
-//     let newRobotDiv = newRobotSection.appendChild(document.createElement("div"));
-//     newRobotDiv.className = "robot-drawing";
-//     newRobotDiv.id = "robot-drawing";
-//     newRobotSection.innerHTML = `<div class="ribbon-vertical">
-//     <div id="ribbon-text"> ${robotche.type} </div> </div>`;
-//     newRobotSection.innerHTML += `<div class="robot-container">`;
-//     newRobotSection.innerHTML += `<div class="robot-head">
-//     <div class="mouth"></div>
-
-//     <div class="eyes">
-//       <div class="eyes-1"></div>
-//       <div class="eyes-2"></div>
-//     </div>
-//   </div>` ;
-   
-//     });
-  
-// }
-// }
-
-
+      if (robotche.type === "female-robot" || robotche.type === "Female") {
+        let newRobotSection = document.createElement("section");
+        newRobotSection.className = "container-1";
+        newRobotSection.id = `robot-section-${newRobots.indexOf(robotche) + 1}`;
+        body.appendChild(newRobotSection);
+        body.insertBefore(newRobotSection, robotSectionOne);
+        let newRobotDiv = newRobotSection.appendChild(
+          document.createElement("div")
+        );
+        newRobotDiv.className = "testche";
+        newRobotSection.innerHTML += `
+      <div class="ribbon-vertical">
+      <div id="ribbon-text">${robotche.type}</div></div>
+      <div class="robot-container">
+        <div class="robot-drawing">
+          <div class="robot-head">
+            <div class="mouth"></div>
+            <div class="eyes">
+              <div class="eyes-1"></div>
+              <div class="eyes-2"></div>
+            </div>
+          </div>
+          <div class="chest-container">
+            <div class="left-arm"></div>
+            <div class="body" style="
+              border-top: 10vmin solid ${robotche.color};
+              border-left: 2vmin solid transparent;
+              border-right: 2vmin solid transparent;
+            }"></div>
+            <div class="right-arm"></div>
+          </div>
+          <div class="skirt"></div>
+          <span class="left-leg-skirt"> </span>
+          <span class="right-leg-skirt"> </span>
+         
+            <h4 id="robot-name-h4" style="margin-top: 1rem">${robotche.name}</h4>
+          
+          
+          
+        </div>
+      </div>
+      <div class="paragraphs">
+        <section>
+          <p id="para-1">
+            <strong>P</strong>aragraph text here. Text-align on left, 3% space from right border.
+            Text is only 2 lines and ends with three dots ... Because its too
+            long. On hover full text is shown as tooltip. First letter is bold.
+          </p>
+          <p id="para-2">
+            <span id="italic">Paragraph</span> text here. Text justified, 6% space from right border.
+            Text is only 2 lines and ends with three dots ... Because its too
+            long. On hover full text is shown. First word is Italic.
+          </p>
+          <p id="para-3">
+            <img src="./images/goodluck.jpg" alt="Goodluck Image" class="goodluck-img" />
+            Paragraph text here. Text-align on left, 9 % from right border.
+            Image on left side.
+          </p>
+         
+        </section>
+        
+      </div>
+      
+    </section>
+      `;
+      // Ако роботчето е мъжко
+      } else {
+        let newRobotSection = document.createElement("section");
+        newRobotSection.className = "container-1";
+        newRobotSection.id = `robot-section-${newRobots.indexOf(robotche) + 1}`;
+        body.appendChild(newRobotSection);
+        body.insertBefore(newRobotSection, robotSectionOne);
+        let newRobotDiv = newRobotSection.appendChild(
+          document.createElement("div")
+        );
+        newRobotDiv.className = "testche";
+        newRobotSection.innerHTML += `
+    <div class="ribbon-vertical">
+    <div id="ribbon-text">${robotche.type}</div></div>
+    <div class="robot-container">
+      <div class="robot-drawing">
+        <div class="robot-head">
+          <div class="mouth"></div>
+          <div class="eyes">
+            <div class="eyes-1"></div>
+            <div class="eyes-2"></div>
+          </div>
+        </div>
+        <div class="chest-container">
+          <div class="left-arm"></div>
+          <div class="body" style="
+            border-top: 10vmin solid ${robotche.color};
+            border-left: 2vmin solid transparent;
+            border-right: 2vmin solid transparent;
+          }"></div>
+          <div class="right-arm"></div>
+        </div>
+        <div class="legs">
+          <div class="left-leg"></div>
+          <div class="right-leg"></div>
+        </div>
+       
+          <h4 id="robot-name-h4">${robotche.name}</h4>
+        
+        
+        
+      </div>
+    </div>
+    <div class="paragraphs">
+      <section>
+        <p id="para-1">
+          <strong>P</strong>aragraph text here. Text-align on left, 3% space from right border.
+          Text is only 2 lines and ends with three dots ... Because its too
+          long. On hover full text is shown as tooltip. First letter is bold.
+        </p>
+        <p id="para-2">
+          <span id="italic">Paragraph</span> text here. Text justified, 6% space from right border.
+          Text is only 2 lines and ends with three dots ... Because its too
+          long. On hover full text is shown. First word is Italic.
+        </p>
+        <p id="para-3">
+          <img src="./images/goodluck.jpg" alt="Goodluck Image" class="goodluck-img" />
+          Paragraph text here. Text-align on left, 9 % from right border.
+          Image on left side.
+        </p>
+       
+      </section>
+      
+    </div>
+    
+  </section>
+    `;
+      }
+    });
+  }
+}
